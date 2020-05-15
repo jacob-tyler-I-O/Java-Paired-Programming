dbaf2158ad7187b71694176e3735a2e63e0d0dc4
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-05-15 16:12:46 -0500

    created contact constructor

---
+++ src/Contact.java
@@ -0,0 +1,13 @@
+public class Contact {
+
+    String firstName;
+    String lastName;
+    Long phoneNumber;
+
+    public Contact (String firstName, String lastName, Long phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+}
