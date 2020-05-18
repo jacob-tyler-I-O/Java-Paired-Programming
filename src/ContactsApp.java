c1b2bf8d0be7f52a526134311f11262e35ce7d11
Jacob Hensley <jacobrhensley3@gmail.com>
2020-05-18 13:46:40 -0500

    Created add a contact for contactMenu

--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -17,17 +17,19 @@ public class ContactsApp {
     public static void main(String[] args) {
         ContactsApp app = new ContactsApp();
         app.contactsMenu();
-        app.addContact();
 
     }
 
 
-    public void addContact() {
+    public void addContact(String firstName, String lastName, String phoneNumber) {
         src.Contact jacob = new src.Contact("Jacob", "Hensley", "1234567890");
         src.Contact tyler = new src.Contact("Tyler", "Trudgeon", "0987654321");
+        src.Contact contactName = new src.Contact(firstName, lastName, phoneNumber);
 
         contactsList.add(jacob.contactCreator());
         contactsList.add(tyler.contactCreator());
+        contactsList.add(contactName.contactCreator());
+
 
         writeFile(contactsFilePath, contactsList);
     }
@@ -35,16 +37,27 @@ public class ContactsApp {
 
     public void contactsMenu(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println(
+        System.out.print("\n" +
                 "1. View contacts.\n" +
                 "2. Add a new contact.\n" +
                 "3. Search a contact by name.\n" +
                 "4. Delete an existing contact.\n" +
                 "5. Exit.\n" +
-                "Enter an option (1, 2, 3, 4 or 5):");
+                "Enter an option (1, 2, 3, 4 or 5): \n");
         int menuSelect = scanner.nextInt();
             if (menuSelect == 1) {
                 viewContacts(contactsFilePath, true);
+                contactsMenu();
+            }
+            if (menuSelect == 2){
+                System.out.println("Enter the contact first name here: ");
+                String firstName = scanner.next();
+                System.out.println("Enter the contact last name here: ");
+                String lastName = scanner.next();
+                System.out.println("Enter the contact phone number here: ");
+                String phoneNumber = scanner.next();
+                addContact(firstName, lastName, phoneNumber);
+                contactsMenu();
             }
     }
 
